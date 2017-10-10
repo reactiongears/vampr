fc6c33c0027a6b4528d36276dadfcaa4052cd144
Mike Kerr <reactiongears@gmail.com>
2017-10-09 21:32:40 -0700

    test commit

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,14 @@
 module.exports = {
   NODE_ENV: '"production"'
+
+#Default GitHub
+Host github.com
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/id_rsa
+
+Host github-reactiongears
+  HostName github.com
+  User git
+  IdentityFile ~/.ssh/id_rsa_reactiongears
 }
